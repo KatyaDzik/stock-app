29c8c14e39eeb5380034941f4239eaf88654ad47
KatyaDzik <ya.dzik01@gmail.com>
2023-02-22 12:54:32 +0300

    add php-doc for UserDto

--- src/app/Dto/UserDto.php
+++ src/app/Dto/UserDto.php
@@ -9,6 +9,12 @@ class UserDto
     private int $role_id;
     private string $password;
 
+    /**
+     * @param string $name
+     * @param string $login
+     * @param int $role_id
+     * @param string $password
+     */
     public function __construct(string $name, string $login, int $role_id, string $password)
     {
         $this->name = $name;
@@ -17,21 +23,33 @@ class UserDto
         $this->password = $password;
     }
 
+    /**
+     * @return string
+     */
     public function getName(): string
     {
         return $this->name;
     }
 
+    /**
+     * @return string
+     */
     public function getLogin(): string
     {
         return $this->login;
     }
 
+    /**
+     * @return int
+     */
     public function getRole(): int
     {
         return $this->role_id;
     }
 
+    /**
+     * @return string
+     */
     public function getPassword(): string
     {
         return $this->password;
